5fe8e8cb31bac416dbda6b207dfd3820d1f966b5
strychnide <thereap3r97@gmail.com>
2019-04-01 11:49:02 +0200

    provide pypy linux wheels

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setup(
     name='libmediainfo_cffi',
-    version='2.1.0',
+    version='2.1.1',
     author='Alessandro Cerruti',
     author_email='thereap3r97@gmail.com',
     description='CFFI interface for libmediainfo',
